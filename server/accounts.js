fe9ccf45fb899d8cae415b9df4f17d228f5b7b1d
thmslmr <thomas.lemeur@edu.esiee.fr>
2016-05-18 16:17:58 +0200

    Configuration google  + facebook

---
+++ server/accounts.js
@@ -0,0 +1,17 @@
+ServiceConfiguration.configurations.remove({
+	service: 'facebook'
+});
+ServiceConfiguration.configurations.insert({
+		  service: 'facebook',
+		  appId: 507758349412679,
+		  secret: b9f783f35fc11d6dbda3a5c050f58401,
+});
+
+ServiceConfiguration.configurations.remove({
+	service : 'google'
+})
+ServiceConfiguration.configurations.insert({
+		  service: 'google',
+		  clientId: 224132618328-hc2128g4d8frn2d42gmop7243a63e3t2.apps.googleusercontent.com,
+		  secret: cLDbqD6BUfcqvzpqfEU6FbFc,
+});
