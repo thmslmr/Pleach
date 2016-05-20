df9624ef944fb0142ab6f2b3560284e616bf145c
thmslmr <thomas.lemeur@edu.esiee.fr>
2016-05-20 16:28:29 +0200

    Amélioration methode addLesson
    Ajout des champs + verification des données avant insertion dans la bdd + verification de la connexion d'un utilisateur

--- client/controllers/addLesson.js
+++ client/controllers/addLesson.js
@@ -1,14 +1,26 @@
 Template.addLesson.events({
   'click .js-submit' : function(evt){
-     title = $('.js-title').val();
-     name = $('.js-name').val();
-     desc = $('.js-desc').val();
 
-     Meteor.call('addLesson', title, name , desc, function(err){
+    LessonObject = {
+      title : $('.js-title').val(),
+      level : $('.js-level').val(),
+      price : parseInt($('.js-price').val()),
+      categorie : $('.js-categorie').val(),
+      description : $('.js-desc').val(),
+      nbseats : $('.js-nbseats').val(),
+      address : {
+        street : $('.js-street').val(),
+        city : $('.js-city').val(),
+        country : $('.js-country').val()
+      },
+      date : $('.js-date').val()
+    }
+
+    Meteor.call('addLesson', LessonObject, function(err){
        if(err){
          console.log(err.reason)
        }
-     });
+    });
 
   }
 })
