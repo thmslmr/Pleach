40132327f9db3c109e78b9f15a5ad75823ca5a2e
thmslmr <thomas.lemeur@edu.esiee.fr>
2016-05-28 11:45:43 +0200

    Finish Feature
    - Ajout du package email
    - Ajout du package SSR (Server Side Rendering)
    - Création d'un template (privé) pour l'email de bienvenue.
    - Création de la fonction d'envoie d'un email de bienvenue à un utilisateur. 
    - Appel de cette fonction à la création d'un utilisateur.

--- server/accounts.js
+++ server/accounts.js
@@ -21,8 +21,10 @@ ServiceConfiguration.configurations.insert({
 //Mise en forme des données utilisateurs récupérées depuis le service tiers
 Accounts.onCreateUser(function(options, user){
 
+	// Récupération du service utilisé (fb / google)
 	service = Object.keys(user['services'])[0]
 
+	// Mise en forme des données utilisateur dans l'objet profile
 	if(service == 'google'){
 		options.profile.picture = user.services.google.picture;
 		options.profile.email = user.services.google.email;
@@ -37,7 +39,11 @@ Accounts.onCreateUser(function(options, user){
 		options.profile.gender = user.services.facebook.gender;
 	}
 
+	//  Mise à jour du profile de l'utilisateur
 	user.profile = options.profile
+
+	// Appel de la fonction d'envoie d'email de bienvenue.
+	sendWelcomeEmail( user.profile );
 
     return user;
 });
