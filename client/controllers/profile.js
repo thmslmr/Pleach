a46b239bd64c5f04a5c05b84a85bd16daba43555
thmslmr <thomas.lemeur@edu.esiee.fr>
2016-06-02 18:45:51 +0200

    Notice profile
    - Affichage des avis 
    - Affichage note moyenne + nombre d'avis

--- client/controllers/profile.js
+++ client/controllers/profile.js
@@ -13,7 +13,32 @@ Template.profile.events({
 // Helpers pour le template addLesson
 Template.profile.helpers({
     'userLessons' : function(){
-        return Lessons.find({},{sort : {'private.createdAt' : -1}});
+        return Lessons.find({'private.owner' : Meteor.userId()},{sort : {'private.createdAt' : -1}});
     },
+    'userNotices' : function(){
+        lessons = Lessons.find({'private.owner' : Meteor.userId()});
+        notices = []
+        lessons.forEach(function(el){
+            notices.push(el.private.notice)
+        })
 
+        return _.flattenDeep(notices)
+    },
+    'nbNotices' : function(){
+        lessons = Lessons.find({'private.owner' : Meteor.userId()});
+        notices = []
+        lessons.forEach(function(el){
+            notices.push(el.private.notice)
+        })
+        return _.flattenDeep(notices).length
+    },
+    'average' : function(){
+        lessons = Lessons.find({'private.owner' : Meteor.userId()});
+        notices = []
+        lessons.forEach(function(el){
+            notices.push(el.private.notice)
+        })
+
+        return _.meanBy( _.flattenDeep(notices), function(el) { return el.grade} );
+    }
 });
