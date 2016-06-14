4b7365dea1ddd94ef1172320b358a8c04418a464
thmslmr <thomas.lemeur@edu.esiee.fr>
2016-06-14 18:03:22 +0200

    AddLesson UI

--- client/controllers/addLesson.js
+++ client/controllers/addLesson.js
@@ -1,3 +1,5 @@
+step_nb = 1;
+
 // Fonction exécutée au rendu du template addLesson
 Template.addLesson.onRendered(function(){
 
@@ -9,49 +11,60 @@ Template.addLesson.onRendered(function(){
         }
     });
 
-    $('.step-form').hide();
-
 });
 
 // Gestion des evenements pour le template addLesson
 Template.addLesson.events({
 
-  'click .js-submit' : function(evt){
+    'click .js-submit' : function(evt){
 
-    $('.js-address').trigger('geocode');
+        $('.js-address').trigger('geocode');
 
-    LessonObject = {
-        title : _.trim( $('[data-newLesson = "title"]').val() ),
-        description : _.trim( $('[data-newLesson = "description"]').val() ),
-        categorie : _.trim( $('[data-newLesson = "categorie"]').val() ),
-        level : parseInt( $('[data-newLesson = "level"]').val() ),
-        nbseats : parseInt( $('[data-newLesson = "nbseats"]').val() ),
-        date : moment( $('[data-newLesson = "date"]').val() )._d,
-        price : parseInt( $('[data-newLesson   ="price"]').val() ),
-        duration : _.trim( $('[data-newLesson = "duration"]').val() ),
-        timestart : _.trim( $('[data-newLesson = "timestart"]').val() )
-    };
+        LessonObject = {
+            title : _.trim( $('[data-newLesson = "title"]').val() ),
+            description : _.trim( $('[data-newLesson = "description"]').val() ),
+            categorie : _.trim( $('[data-newLesson = "categorie"]').val() ),
+            level : parseInt( $('[data-newLesson = "level"]').val() ),
+            nbseats : parseInt( $('[data-newLesson = "nbseats"]').val() ),
+            date : moment( $('[data-newLesson = "date"]').val() )._d,
+            price : parseInt( $('[data-newLesson   ="price"]').val() ),
+            duration : _.trim( $('[data-newLesson = "duration"]').val() ),
+            timestart : _.trim( $('[data-newLesson = "timestart"]').val() )
+        };
 
-    LessonObject.address = {
-        name : address_object.formatted_address,
-        loc : {
-            type : "Point",
-            coordinates : [ address_object.geometry.location.lng() , address_object.geometry.location.lat() ]
+        LessonObject.address = {
+            name : address_object.formatted_address,
+            loc : {
+                type : "Point",
+                coordinates : [ address_object.geometry.location.lng() , address_object.geometry.location.lat() ]
+            }
+
+        };
+
+        // Appel de la méthode d'ajout d'un cours
+        Meteor.call('addLesson', LessonObject, function(err){
+            if(err){
+                console.log(err);
+            }else{
+                Router.go('home');
+            }
+        });
+
+    },
+
+    'click .js-next' : function(evt){
+        if(step_nb == 3)
+        return;
+
+        step_nb += 1;
+
+        dist = parseInt($('.step-content:nth-child(' + step_nb + ')').offset().left) - 50;
+        $('.all-container').velocity({'translateX' : '-=' + dist}, {'easing' : 'ease-out'});
+        $('.step:nth-child(' + step_nb + ')').addClass('step--done');
+        if(step_nb == 3){
+            $('.end-form a').addClass('js-submit').removeClass('js-next').text("C'est parti !");
         }
-
-    };
-
-    // Appel de la méthode d'ajout d'un cours
-    Meteor.call('addLesson', LessonObject, function(err){
-        if(err){
-            console.log(err);
-        }else{
-            Router.go('home');
-        }
-    });
-
-  }
-
+    }
 });
 
 // Helpers pour le template addLesson
